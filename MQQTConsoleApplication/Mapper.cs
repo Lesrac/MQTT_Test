fb2015f2237c221cd7d8af973cc2fd98050bd63f
Lesrac <danielfrick@bluewin.ch>
2015-09-29 19:09:49 +0200

    MQQT Initial

---
+++ MQQTConsoleApplication/Mapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MQQTConsoleApplication
+{
+    class Mapper
+    {
+        readonly static string MAPPINGFILE = "config/MapConfig.txt";
+        readonly static string VIEWPATH = "views/";
+        static Dictionary<string, string> mappingDictionary;
+
+        private Mapper()
+        {
+
+        }
+
+        public static void buildDictionary()
+        {
+            mappingDictionary = new Dictionary<string, string>();
+            string line;
+
+            StreamReader file = new StreamReader(MAPPINGFILE);
+            while((line = file.ReadLine()) != null)
+            {
+                string[] splitted = line.Split(':');
+                Console.WriteLine(splitted[0]);
+                Console.WriteLine(splitted[1]);
+                mappingDictionary.Add(splitted[0].Trim(), splitted[1].Trim());
+            }
+        }
+
+        public static StreamReader getMatchingFile(string key)
+        {
+            if (mappingDictionary.ContainsKey(key))
+            {
+                return new StreamReader(VIEWPATH + mappingDictionary[key]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ MQQTConsoleApplication/Subscriber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQQTConsoleApplication
+{
+    class Subscriber
+    {
+        static string MQTT_BROKER_ADDRESS = "localhost";
+        public static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient(MQTT_BROKER_ADDRESS);
+
+            // register to message received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            client.Subscribe(new string[] { "/testit/now" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine("Received");
+            //  Console.WriteLine(e.Message.);
+            string msg = Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine(msg);
+            Mapper.buildDictionary();
+        }
+
+        static void registerSubscriber(MqttMsgPublishReceived publishReceived)
+        {
+
+        }
+
+    }
+}
